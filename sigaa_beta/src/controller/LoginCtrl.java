6fe01e9df190c36dbf7036454ff431a137a25531
LuizxDev <luizxfelipe.dev@gmail.com>
2023-06-02 03:25:02 -0300

    metodo save
    metodo de salvar a senha e login feito

--- sigaa_beta/src/controller/LoginCtrl.java
+++ sigaa_beta/src/controller/LoginCtrl.java
@@ -1,21 +1,53 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.swing.JOptionPane;
 
+import model.ConexaoBd;
 import model.UsuarioModel;
 
 public class LoginCtrl{
 
+    UsuarioModel info = new UsuarioModel();
+
     public void fazerLogin() {
-        UsuarioModel info = new UsuarioModel();
 
         info.setLogin_usuario(JOptionPane.showInputDialog(null, "Digite o seu login"));
 
         info.setSenha_usuario(JOptionPane.showInputDialog(null, "Digite sua Senha"));
     }
 
-    public void save(UsuarioModel usuario){
-        public String sql = "";
+    public void save(UsuarioModel usuario) throws Exception{
+        String sql = "insert into info(nomeUsuario, senhaUsuario)values (?, ?)";
+
+        Connection conn = null;
+        PreparedStatement pstm = null;
+
+        try {
+            conn = ConexaoBd.criarConexaoBd();
+            pstm = conn.prepareStatement(sql);
+
+            pstm.setString(1, info.getLogin_usuario());
+            pstm.setString(2, info.getSenha_usuario());
+
+            pstm.executeQuery();
+
+        } catch (Exception erro) {
+            // Lidar com a exceção
+            throw erro;
+        }finally{
+            // Fechar a conexão e os recursos
+            if(pstm != null){
+                pstm.close();
+            }
+            if(conn != null){
+                conn.close();
+            }
+        }
+
     }
 
 }
